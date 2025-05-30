f00793fa101c6b7a6c1c467253c2dcec8e469b87
klabak436 <44671022+klabak436@users.noreply.github.com>
2025-05-30 14:45:04 +0000

    Fix: GA script waarschuwing en verwijder testpagina

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"; // Zorg dat deze import er is!
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,24 +20,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="nl">
-      <head>
-        {/* Google Analytics 4 */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZK7H198DET"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-ZK7H198DET');
-            `,
-          }}
-        />
-      </head>
+      {/* Geen <head> tag direct meer hier, Next.js beheert dit met <Script> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        {/* Google Analytics 4 Scripts met next/script componenten */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZK7H198DET"
+          strategy="afterInteractive" // Laadt script na de eerste interactieve elementen
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZK7H198DET'); // Vergeet niet je juiste ID hier te plaatsen!
+          `}
+        </Script>
       </body>
     </html>
   );
